14284473d338dae7199ddbfc4af971b2bcd3aaa4
Andrey Nazarov <mondegor@gmail.com>
2026-01-19 03:08:58 +0300

    integration Remove nopslog

--- mrlog/slog/nopslog/logger.go
+++
@@ -1,8 +0,0 @@
-package nopslog
-
-import "github.com/mondegor/go-sysmess/mrlog/slog"
-
-// New - создаёт объект slog.LoggerAdapter.
-func New() *slog.LoggerAdapter {
-	return slog.New(handler{})
-}
