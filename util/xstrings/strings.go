d1a21262b0eaea05c2e07f5e673da52e9550f4ea
Andrey Nazarov <mondegor@gmail.com>
2026-01-27 02:16:27 +0300

    integration Remove InArray

--- util/xstrings/strings.go
+++ util/xstrings/strings.go
@@ -4,17 +4,6 @@ import (
 	"strings"
 )
 
-// InArray - сообщает есть ли указанная строка в массиве строк.
-func InArray(needle string, haystack []string) bool {
-	for i := range haystack {
-		if haystack[i] == needle {
-			return true
-		}
-	}
-
-	return false
-}
-
 // TrimBeforeSep - возвращает строку без её начальной части продолжающейся до сепаратора включая этот сепаратор.
 // Если сепаратор не был найден, то возвращается исходная строка.
 func TrimBeforeSep(s string, sep byte) string {
